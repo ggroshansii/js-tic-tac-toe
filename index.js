9de440d7cbc5da128b9ffc84704e8eda78626fe6
ggroshansii <garthgroshansii@gmail.com>
2022-03-14 15:31:52 -0400

    create TICTACTOE module with board property, and the starting code for displayBoard method

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-
-function createGameBoard() {
+const TICTACTOE = (function() {
     return {
-        gameBoard: [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null],
-        ]
+        board : [null, null, null, null, null, null, null, null, null],
+        displayBoard(currentBoard) {
+            let cells = document.querySelectorAll(".cell");
+            console.log('cells', cells)
+            for (let i=0; i<cells.length; i++) {
+                cells[i].addEventListener('click', ()=> {
+                    cells[i].textContent = "X";
+                })
+            }
+        }
     }
-}
+})();
 
-function createPlayers() {
-    return {
-        
-    }
-}
+// function createPlayers(numOfPlayers) {
+//     let playersObj = {}
+//     for (let i=0; i<numOfPlayers; i++) {
+//         let input = "What is Player"
+//     }
+//     return playersObj;
+
+// }
+
 
 function game() {
+    let currentBoard = TICTACTOE.board2;
 
+    TICTACTOE.displayBoard(currentBoard)
+
+
+    console.log('fired')
+    
 }
+
+game()
